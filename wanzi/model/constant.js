779efc6463b97a7b0510b607cf5374a9e878fb2f
zero-homoj <victzero@gmail.com>
2015-03-30 11:45:27 +0800

    constant and constDAO and commentDAO

---
+++ wanzi/model/constant.js
@@ -0,0 +1,29 @@
+/**
+ * constant, 变量.
+ */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var config = require('../config').config;
+
+var schema = new Schema({
+
+	name: { //key
+		type: String,
+		index: true,
+		unique: true
+	},
+	val: {
+		type: String
+	},
+
+	createTime: { //创建时间
+		type: Date,
+		default: Date.now
+	},
+	modifyTime: { //修改时间.
+		type: Date,
+		default: Date.now
+	},
+});
+
+mongoose.model('CONSTANT', schema);
